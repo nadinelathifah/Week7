90173e8d4265baf92f280495d53ebd288c6263de
Nadine Lathifah <nadinehendronoto@gmail.com>
2025-03-06 20:41:07 +0300

    EDIT: exception comments

--- exceptions/insufficientfunds.py
+++ exceptions/insufficientfunds.py
@@ -1,10 +1,11 @@
 
-#   Define a custom exception class for insufficient funds:
+#   Define a custom exception class to indicate the user of insufficient funds:
+#   The custom exception inherits from built-in Exception class which enables errors to be raised.
 class InsufficientFundsException(Exception):
-#   Constructor to initialize the exception with a custom message.
+#   Constructor __init__ initializes the exception with a custom message.
+#   message= states that if no message is provided when raising the exception, it will automatically default to the string below:
+#   the custom message overrides the message from Exception class.
     def __init__(self, message= "Invalid withdrawal amount. Unfortunately, you are unable to complete the transaction. Please ensure that you have sufficient funds in your account."):
-#   Call the parent constructor to set the message
+#   Call the parent constructor to override the parameter "message"
         super().__init__(message)
         self.message = message
-
-
--- exceptions/withdrawal_attempts.py
+++ exceptions/withdrawal_attempts.py
@@ -1,6 +1,5 @@
 
 class WithdrawalLimitException(Exception):
     def __init__(self, message="★ Withdrawal Blocked ★ Unfortunately, you have reached the maximum withdrawal limit for this month.\nPlease refer to your checking account for future transactions."):
-        #   Call the parent constructor to set the message
         super().__init__(message)
         self.message = message
